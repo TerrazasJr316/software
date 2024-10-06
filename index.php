7de0f620ac648b0d2a51db94ec866f10cb5a00f2
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-10-06 10:55:39 -0600

    hola mundo | 6 - Oct

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
     <title>Mi primera aplicacion con PHP</title>
 </head>
 <body>
-    
+    <h1>Soy un encabezado de HTML</h1>
+    <?php
+        #todo esto es un Script en PHP
+        //También es un comentario de una linea
+        /*Un comentario
+        en multiples
+        lineas
+        */
+
+        echo "<h2>Hola Mundo desde PHP</2>";
+    ?>
 </body>
 </html>
