b4e59a5488f1400d8ea83d4b1cce64f8330d8b34
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-10-06 11:37:45 -0600

    formularios | 6 - Oct

--- proyecto/index.php
+++ proyecto/index.php
@@ -1,28 +1,38 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mi primera aplicacion con PHP</title>
 </head>
+
 <body>
     <h1>Soy un encabezado de HTML</h1>
     <?php
-        #todo esto es un Script en PHP
-        //También es un comentario de una linea
-        /*Un comentario
+    #todo esto es un Script en PHP
+    //También es un comentario de una linea
+    /*Un comentario
         en multiples
         lineas
         */
 
-        # laas variables simpre comienzan con $ 
-        # PHP no es fuertemente tipado, el tipo de dato se crea con la asignación
-        $nombre = "TESJI";
-        $mensaje = "Mi primera aplicación en PHP";
+    # laas variables simpre comienzan con $ 
+    # PHP no es fuertemente tipado, el tipo de dato se crea con la asignación
+    $nombre = "TESJI";
+    $mensaje = "Mi primera aplicación en PHP";
 
-        echo "<h2>Hola Mundo desde PHP</2>";
+    echo "<h2>Hola Mundo desde PHP</2>";
 
-        echo "<h3> Bienvenido $nombre </h3> <h4>$mensaje</h4>";
+    echo "<h3> Bienvenido $nombre </h3> <h4>$mensaje</h4>";
     ?>
+
+    <ul>
+        <a href="pagina2.php">
+            <li>Formularios</li>
+        </a>
+    </ul>
+
 </body>
+
 </html>
--- proyecto/pagina2.php
+++ proyecto/pagina2.php
@@ -1,15 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP</title>
 </head>
+
 <body>
+    <section>
+        <h1>Formularios con PHP</h1>
+        <form>
+            <input type="text" placeholder="Nombre" name="txtName">
+            <input type="number" placeholder="Edad" name="txtAge">
+            <select name="" id="">
+                <option value="">--Seleccione Carrera--</option>
+                <option value="">Ing. Sistemas</option>
+                <option value="">Ing. Mecatronica</option>
+                <option value="">Ing. Logistíca</option>
+                <option value="">Ing. Industrial</option>
+                <option value="">Ing. Civil</option>
+            </select>
+            <button>Registrar</button>
+            <button>Nuevo</button>
+        </form>
+    </section>
     <?php
 
     echo "PÁGUINA 2";
-    
+
     ?>
 </body>
+
 </html>
